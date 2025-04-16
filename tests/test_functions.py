ebc5e6e3a8f7ad6d4f041d1fce550b87661cfb32
JulianKimmig <julian.kimmig@gmx.net>
2025-04-16 06:34:40 +0200

    Refactor code structure for improved readability and maintainability

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,30 +1,52 @@
-import unittest
 from funcnodes_chemprop import functions as fn
 import pandas as pd
 import os
-from lightning import pytorch as pl
 from chemprop import models
+import pytest
 
 
-class TestFunctions(unittest.TestCase):
-    def test_train(self):
-        df = pd.read_csv(os.path.join(os.path.dirname(__file__), "mol.csv"))
-        smiles_column = "smiles"
-        target_columns = "lipo"
-        train_loader, val_loader, test_loader, scaler = fn.make_data(
-            df, smiles_column, target_columns
-        )
-        model = fn.make_model(scaler)
+@pytest.mark.asyncio
+async def test_chemprop_workflow():
+    df = pd.read_csv(os.path.join(os.path.dirname(__file__), "mol.csv"))
+    smiles_column = "smiles"
+    target_columns = ["lipo"]
 
-        self.assertIsInstance(
-            model,
-            models.MPNN,
-            f"Model is not a MPNN but {type(model)}",
-        )
+    (
+        train_loader,
+        val_loader,
+        test_loader,
+        scaler,
+        train_indices,
+        val_indices,
+        test_indices,
+    ) = await fn.make_data(
+        df, smiles_column, target_columns, split=(0.8, 0.1, 0.1), in_new_thread=True
+    )
 
-        self.assertIsInstance(
-            model,
-            pl.LightningModule,
-            f"Model is not a LightningModule but {type(model)}",
-        )
-        _ = fn.predict(model, df[smiles_column])
+    model = fn.make_model(scaler)
+    inipreds = await fn.predict(model, df[smiles_column], in_new_thread=False)
+
+    initerror = ((df[target_columns].values - inipreds) ** 2).mean() ** 0.5
+
+    assert initerror > 1, "Initial RMSE should be greater than 1"
+
+    assert isinstance(
+        model,
+        models.MPNN,
+    )
+
+    import time
+
+    start_time = time.time()
+    async for metrics in fn.train_in_subprocess(
+        model, train_loader, val_loader, test_loader, max_epochs=20
+    ):
+        print("\n", time.time() - start_time)
+
+    model, results = metrics
+    preds = await fn.predict(model, df[smiles_column], in_new_thread=False)
+
+    # rmse
+    error = ((df[target_columns].values - preds) ** 2).mean() ** 0.5
+
+    assert error < initerror, "RMSE should be less than initial RMSE"
